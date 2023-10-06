ae2bbc30e77e92592c69bf48152b615f17790b50
Berran01 <91222375+Berran01@users.noreply.github.com>
2023-10-06 13:20:58 +0200

    Add files via upload

--- lab3robot/main.m
+++ lab3robot/main.m
@@ -9,13 +9,33 @@ n = 1/20;
 
 [J,umax] = lab3robot(010727);
 
-numerator = Kt*n;         
-denominator = [0, b*Rm*(1+Km), (J*Rm+b*Lm)*(1+Km),J*Lm*(1+Km)];
+s = tf("s");
 
-G = tf(numerator, denominator);
+G0 = 1/(J*s+b) * Kt * 1/(s*Lm + Rm);
+
+G = n/s * G0 * 1/(1+Km*G0);
 
 lab3robot(G,010727)
 
 %% 3.2
 
-bode(G)
+K = 226;
+G_cl = K * G/(1+K*G);
+
+[y, t] = step(G_cl);
+plot(t, y);
+title('Step Response of Closed-Loop System');
+xlabel('Time');
+ylabel('Response');
+grid on;
+
+%% 3.3
+
+bode(K*G)
+grid on;
+w_b = bandwidth(K*G);
+[GM, PM, w_c] = margin(K*G);
+
+%% 3.4
+
+
